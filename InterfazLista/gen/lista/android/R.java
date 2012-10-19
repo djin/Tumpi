917322b467b7fb84e4009f79f94b060dde784371
Sergio Redondo <zell.y.algo@gmail.com>
2012-10-19 11:47:36 +0200

    [PROTO]Interfaz con botones favorito y compratir

--- InterfazLista/gen/lista/android/R.java
+++ InterfazLista/gen/lista/android/R.java
@@ -14,11 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnSettings=0x7f080004;
-        public static final int btnVotar=0x7f080002;
-        public static final int songName=0x7f080001;
+        public static final int btnFav=0x7f080001;
+        public static final int btnSettings=0x7f080006;
+        public static final int btnVotar=0x7f080004;
+        public static final int btnshare=0x7f080002;
+        public static final int songName=0x7f080003;
         public static final int txtPlaying=0x7f080000;
-        public static final int txtSetting=0x7f080003;
+        public static final int txtSetting=0x7f080005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -30,10 +32,12 @@ public final class R {
         public static final int menuactionbar=0x7f070000;
     }
     public static final class raw {
-        public static final int ico_big_love=0x7f040000;
-        public static final int ico_medium_list=0x7f040001;
+        public static final int ico_medium_list=0x7f040000;
+        public static final int ico_medium_love=0x7f040001;
         public static final int ico_medium_options=0x7f040002;
         public static final int ico_medium_star=0x7f040003;
+        public static final int ico_small_love=0x7f040004;
+        public static final int ico_small_share=0x7f040005;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
