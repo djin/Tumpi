40dba7b98e99f56c88b7a63beb536e462fc534c3
Fancisco Javier Arribas Fernández <mdo@arribasjavier.com>
2012-10-26 12:55:38 +0200

    Creacion del proyecto de prueba de socket en servidor

---
+++ PruebaSocketServidor/src/Prueba/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prueba;
+import java.io.IOException;
+import java.net.ServerSocket;
+/**
+ *
+ * @author 66785270
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        ServerSocket prueba=new ServerSocket(22222);
+    }
+}
