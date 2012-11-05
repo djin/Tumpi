699ea51788972fbfeb4cbb56810010be60ff2410
Fancisco Javier Arribas Fernández <mdo@arribasjavier.com>
2012-11-05 22:21:26 +0100

    Ejemplo de sockets terminado

--- PruebaSocketCliente/src/Prueba/Main/MainActivity.java
+++ PruebaSocketCliente/src/Prueba/Main/MainActivity.java
@@ -4,37 +4,76 @@ package Prueba.Main;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.*;
+import android.widget.EditText;
 import android.widget.TextView;
 import java.io.*;
 import java.net.*;
 import java.net.Socket.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class MainActivity extends Activity
 {
     /** Called when the activity is first created. */
+    TextView texto_main=null;
+    EditText input_mensaje=null;
+    char[] texto_mensaje = null;
+    Socket cliente=null;
+    InputStream input = null;
+    OutputStream output = null;
+    DataInputStream inputdata = null;
+    DataOutputStream outputdata = null;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        TextView label=(TextView) findViewById(R.id.main);
-        label.setText("Adios");
-        try {
-            SocketAddress dir_server=new InetSocketAddress("192.168.173.1",22222);
-            Socket cliente=new Socket();
-            do{
-                cliente.connect(dir_server);
-            }while(!cliente.isConnected());
-            
-            InputStream input = cliente.getInputStream();
-            OutputStream output = cliente.getOutputStream();
-            DataInputStream inputdata = new DataInputStream (input);
-            DataOutputStream outputdata = new DataOutputStream (output);
-            outputdata.writeUTF("HOLA MUNDO");
+        texto_main=(TextView) findViewById(R.id.texto_log);
+        input_mensaje=(EditText) findViewById(R.id.mensaje);
+        texto_main.setText("Conéctate");
+        
+    }
+    public void conectar(View view) {
+        try {            
+            cliente = new Socket("192.168.1.24",22222);
+            if(cliente.isConnected())
+                texto_main.append("\nConexión realizada con éxito!!");
         } catch (Exception ex) {
-             label.setText(ex.toString());
-        }  
+             texto_main.append("\nError de conexion: "+ex.toString());
+        }
+    }
+    public void enviarMensaje(View view) {
+        if(cliente.isConnected() && !cliente.isClosed()){
+            try {
+                input = cliente.getInputStream();
+                output = cliente.getOutputStream();
+                inputdata = new DataInputStream (input);
+                outputdata = new DataOutputStream (output);
+                Editable datos=input_mensaje.getText();
+                texto_mensaje=new char[250];
+                datos.getChars(0, datos.length(), texto_mensaje,0);
+                String mensaje=new String(texto_mensaje);
+                outputdata.writeUTF(mensaje);
+                input_mensaje.setText("");
+                outputdata.flush();
+            } catch (IOException ex) {
+               texto_main.append("\nError al enviar mensaje: "+ex.toString());
+            }
+        }
+        else
+            texto_main.append("\nNo esta conectado al servidor!!!");
+    }
+    public void cerrarConexion(View view) {
+        if(cliente.isConnected() && !cliente.isClosed()){
+            try {
+                cliente.close();
+            } catch (IOException ex) {
+                texto_main.append("\nError al desconectar: "+ex.toString());
+            }
+        }
+        else
+            texto_main.append("\nNo esta conectado al servidor!!!");
+    }
+    public void limpiarLog(View view) {
+        texto_main.setText("Conéctate!!");
     }
 }
--- PruebaSocketServidor/src/Prueba/Main.java
+++ PruebaSocketServidor/src/Prueba/Main.java
@@ -13,41 +13,58 @@ import java.util.logging.Logger;
  * @author 66785270
  */
 public class Main {
-
+    static ServerSocket server=null;
+    static Socket socket_cliente=null;
+    static Runnable escucharClientes=new Runnable() {
+        @Override
+        public void run() {
+            try {
+                socket_cliente=server.accept();
+                System.out.println("Cliente conectado: "+socket_cliente.getInetAddress());
+                atenderCliente(socket_cliente);
+                escucharClientes.run();
+            } catch (IOException ex) {
+                System.out.println("Error al escuchar clientes: "+ex.toString());
+            }
+        }
+    };
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         // TODO code application logic here
-        ServerSocket server=new ServerSocket(22222);
-       /* new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    SocketAddress dir_server=new InetSocketAddress("192.168.173.1",22222);
-                    Socket cliente=new Socket();
-                    do{
-                        cliente.connect(dir_server);
-                    }while(!cliente.isConnected());
-                    InputStream input = cliente.getInputStream();
-                    OutputStream output = cliente.getOutputStream();
-                    DataInputStream inputdata = new DataInputStream (input);
-                    DataOutputStream outputdata = new DataOutputStream (output);
-                    outputdata.writeUTF("HOLA MUNDO");
-                    
-                } catch (Exception ex) {
-                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                }                
+        try {
+            server = new ServerSocket(22222);
+            if(server.isBound()){
+                System.out.println("Socket creado con éxito");
+                escucharClientes.run();
             }
-        }.run();*/
-        System.out.println(server.getInetAddress());
-        Socket socket_cliente=server.accept();
-        InputStream input = socket_cliente.getInputStream();
-        OutputStream output = socket_cliente.getOutputStream();
-        DataInputStream inputdata = new DataInputStream (input);
-        DataOutputStream outputdata = new DataOutputStream (output);
-        System.out.println(inputdata.readUTF());
-        server.close();
-        //System.out.println(socket_cliente.getChannel());
+        } catch (IOException ex) {
+            System.out.println("Error al crear el socket: "+ex.toString());
+        }
+        
+    }
+    static void atenderCliente(Socket socket_cliente){
+        InputStream input = null;
+        String texto_recivido="";
+        try {
+            input = socket_cliente.getInputStream();
+            OutputStream output = socket_cliente.getOutputStream();
+            DataInputStream inputdata = new DataInputStream (input);
+            DataOutputStream outputdata = new DataOutputStream (output);            
+            do{
+                    texto_recivido=inputdata.readUTF();
+                    System.out.println(texto_recivido);
+            }while(!texto_recivido.equals("exit"));
+        } catch (IOException ex) {
+            System.out.println("Cliente desconectado: "+socket_cliente.getInetAddress());
+        } finally {
+            try {
+                input.close();
+                socket_cliente.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 }
