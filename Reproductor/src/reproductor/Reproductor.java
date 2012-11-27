44de0ac6bb0f2ecae5a6155772570a9f7d8c42d3
Carlos Gomez <superdjin@gmail.com>
2012-11-27 12:16:42 +0100

    Mas pruebas Vlcj

--- Reproductor/src/reproductor/Reproductor.java
+++ Reproductor/src/reproductor/Reproductor.java
@@ -6,10 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import uk.co.caprica.vlcj.binding.internal.libvlc_media_t;
 import uk.co.caprica.vlcj.medialist.MediaList;
+import uk.co.caprica.vlcj.player.MediaPlayer;
+import uk.co.caprica.vlcj.player.MediaPlayerEventAdapter;
 import uk.co.caprica.vlcj.player.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 import uk.co.caprica.vlcj.player.list.MediaListPlayer;
@@ -22,64 +25,83 @@ import uk.co.caprica.vlcj.player.list.MediaListPlayerMode;
  */
 public class Reproductor extends configVlcj {
 
-  public static void main(String[] args) throws Exception {
-    MediaPlayerFactory mediaPlayerFactory = new MediaPlayerFactory();
-    
-    EmbeddedMediaPlayer mediaPlayer = mediaPlayerFactory.newEmbeddedMediaPlayer();
-    
-    final MediaListPlayer mediaListPlayer = mediaPlayerFactory.newMediaListPlayer();
-    
-    mediaListPlayer.addMediaListPlayerEventListener(new MediaListPlayerEventAdapter() {
-      @Override
-      public void nextItem(MediaListPlayer mediaListPlayer, libvlc_media_t item, String itemMrl) {
-        System.out.println("nextItem()");
-      }
-    });
-    
-    mediaListPlayer.setMediaPlayer(mediaPlayer); // <--- Important, associate the media player with the media list player
-    
-    JButton siguiente = new JButton("Siguiente");
-    siguiente.addActionListener(new ActionListener() {
+    private static MediaPlayerFactory mediaPlayerFactory;
+    private static EmbeddedMediaPlayer mediaPlayer;
+    private static MediaListPlayer mediaListPlayer;
+    private static JLabel nombreCancion;
+    private static JLabel artistaCancion;
+
+    public static void main(String[] args) throws Exception {
+
+        mediaPlayerFactory = new MediaPlayerFactory();
+
+        mediaPlayer = mediaPlayerFactory.newEmbeddedMediaPlayer();
+
+        mediaListPlayer = mediaPlayerFactory.newMediaListPlayer();
+
+        mediaListPlayer.addMediaListPlayerEventListener(new MediaListPlayerEventAdapter() {
+
+            @Override
+            public void nextItem(MediaListPlayer mediaListPlayer, libvlc_media_t item, String itemMrl) {
+                
+            }
+            
+        });
+
+        mediaListPlayer.setMediaPlayer(mediaPlayer); // <--- Important, associate the media player with the media list player
+
+        JButton siguiente = new JButton("Siguiente");
+        siguiente.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 mediaListPlayer.playNext();
             }
         });
-    
-    JButton pausa = new JButton("Pausa");
-    pausa.addActionListener(new ActionListener() {
+
+        JButton pausa = new JButton("Pausa");
+        pausa.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 mediaListPlayer.pause();
             }
         });
-    
-    JPanel cp = new JPanel();
-    cp.setLayout(new BorderLayout());
-    cp.add(pausa, BorderLayout.CENTER);
-    cp.add(siguiente, BorderLayout.SOUTH);
-    
-    
-    JFrame f = new JFrame("vlcj embedded media list player test");
-    f.setContentPane(cp);
-    f.setSize(200, 100);
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    f.setVisible(true);
-    
-    MediaList mediaList = mediaPlayerFactory.newMediaList();
-    mediaList.addMedia("1.mp3");
-    mediaList.addMedia("2.mp3");
-    
-    mediaListPlayer.setMediaList(mediaList);
-    mediaListPlayer.setMode(MediaListPlayerMode.LOOP);
 
-    mediaListPlayer.play();
+        JPanel panelPrincipal = new JPanel();
+        panelPrincipal.setLayout(new BorderLayout());
+
+        JPanel panelCentral = new JPanel();
+        nombreCancion = new JLabel("-");
+        artistaCancion = new JLabel("-");
+
+        panelCentral.add(nombreCancion);
+        panelCentral.add(artistaCancion);
+
+        JPanel panelBotones = new JPanel();
+        panelPrincipal.add(panelCentral, BorderLayout.CENTER);
+        panelPrincipal.add(panelBotones, BorderLayout.SOUTH);
+        panelBotones.add(pausa);
+        panelBotones.add(siguiente);
+
+        JFrame ventana = new JFrame("Reproductor");
+        ventana.setContentPane(panelPrincipal);
+        ventana.setSize(200, 100);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
+
+        MediaList mediaList = mediaPlayerFactory.newMediaList();
+        mediaList.addMedia("1.mp3");
+        mediaList.addMedia("2.mp3");
+
+        mediaListPlayer.setMediaList(mediaList);
+        mediaListPlayer.setMode(MediaListPlayerMode.LOOP);
+
+        mediaListPlayer.play();
 
 //    mediaList.release();
 //    mediaListPlayer.release();
 //    mediaPlayer.release();
 //    mediaPlayerFactory.release();
-  }
+    }
 }
