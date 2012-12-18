fb77e306aad3c0c5a12a07a55d1ff6345280015c
Carlos Gomez <superdjin@gmail.com>
2012-12-18 16:19:16 +0100

    Metodo getCancion añadido

--- InterfazServidor/src/reproductor/PlayerReproductor.java
+++ InterfazServidor/src/reproductor/PlayerReproductor.java
@@ -1,6 +1,9 @@
 package reproductor;
 
+import java.io.File;
+import modelos.Cancion;
 import uk.co.caprica.vlcj.component.AudioMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.MediaMeta;
 import uk.co.caprica.vlcj.player.MediaPlayer;
 
 
@@ -18,6 +21,7 @@ public class PlayerReproductor extends configVlcj{
     
     public void reproducir(String mrl){
         reproductor.getMediaPlayer().playMedia(mrl);
+        System.out.println(reproductor.getMediaPlayer().getLength());
     }
         
     public static void pausar(){
@@ -26,6 +30,27 @@ public class PlayerReproductor extends configVlcj{
     
     public MediaPlayer getMediaPlayer(){
         return reproductor.getMediaPlayer();
+    }
+    
+    public Cancion getCancion(String mrl){
+        Cancion cancion;
+        MediaMeta metadata;
+        String duracion="";
+        long dur=0,min=0;
+        reproductor.getMediaPlayer().prepareMedia(mrl);
+        reproductor.getMediaPlayer().parseMedia();
+        metadata = reproductor.getMediaPlayer().getMediaMeta();
+//        dur = reproductor.getMediaPlayer().getLength();
+//        System.out.println("dentro"+dur);
+//        dur = dur/1000;
+//        while(dur>59){
+//            dur = dur-60;
+//            min++;
+//        }
+//        duracion = Long.toString(min)+":"+ Long.toString(dur);
+        cancion = new Cancion(0,metadata.getTitle(), metadata.getAlbum(),metadata.getArtist(),duracion, mrl);
+        System.out.println(metadata.getTitle()+" "+metadata.getAlbum()+" "+metadata.getArtist()+" "+duracion+" "+mrl);
+        return cancion; 
     }
     
 }
