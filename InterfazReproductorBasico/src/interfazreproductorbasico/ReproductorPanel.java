18e48721c6947764af8b136ecaf6b1fb4a15be15
Carlos Gomez <superdjin@gmail.com>
2012-12-19 11:41:43 +0100

    retoques interfaz reproductor

--- InterfazReproductorBasico/src/interfazreproductorbasico/ReproductorPanel.java
+++ InterfazReproductorBasico/src/interfazreproductorbasico/ReproductorPanel.java
@@ -2,10 +2,13 @@ package interfazreproductorbasico;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -22,8 +25,17 @@ public class ReproductorPanel extends JPanel {
     private ImageIcon playPress;
     private ImageIcon pauseIcon;
     private ImageIcon pausePress;
+    public static JPanel panelBotones;
+    public static JPanel panelNombres;
+    public static JLabel song;
+    public static JLabel artist;
+    
     
     public ReproductorPanel() {
+        
+        FlowLayout fl = new FlowLayout(FlowLayout.LEFT);
+        this.setLayout(fl);
+        panelBotones = new JPanel(fl);
         
         playIcon = new ImageIcon("icons/play.png");
         pauseIcon = new ImageIcon("icons/pause.png");
@@ -87,9 +99,23 @@ public class ReproductorPanel extends JPanel {
         });
         
         
-        this.add(stop);
-        this.add(play);
-        this.add(next);
+        panelBotones.add(stop);
+        panelBotones.add(play);
+        panelBotones.add(next);
+        this.add(panelBotones);
+        
+        panelNombres = new JPanel();
+        song = new JLabel("Nombre de la canción");
+        song.setFont(new Font("", Font.PLAIN, 14));
+        artist = new JLabel("Artista");
+        artist.setFont(new Font("", Font.PLAIN, 12));
+        panelNombres.setPreferredSize(new Dimension(150,55));
+        FlowLayout fl2 = new FlowLayout(FlowLayout.CENTER,50,10);
+        panelNombres.setLayout(fl2);
+        panelNombres.add(song);
+        panelNombres.add(artist);
+        
+        this.add(panelNombres);
         
     }
     
