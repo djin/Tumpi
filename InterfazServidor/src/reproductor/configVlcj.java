ea3af969fa489bd1fb532ae02b63eedb70d35a39
carlos <carlos@carlos-W76OC.(none)>
2013-03-05 10:57:19 +0100

    modified:   InterfazServidor/fich_selector 	modified:   InterfazServidor/src/reproductor/configVlcj.java

--- InterfazServidor/src/reproductor/configVlcj.java
+++ InterfazServidor/src/reproductor/configVlcj.java
@@ -1,13 +1,6 @@
 package reproductor;
 
-<<<<<<< HEAD
 import uk.co.caprica.vlcj.discovery.NativeDiscovery;
-=======
-import com.sun.jna.Native;
-import uk.co.caprica.vlcj.binding.LibVlc;
-import uk.co.caprica.vlcj.discovery.NativeDiscovery;
-import uk.co.caprica.vlcj.runtime.RuntimeUtil;
->>>>>>> e87b8886bd78854bf46e7f70586bd9ca6aee56ea
 
 public abstract class configVlcj {
 
