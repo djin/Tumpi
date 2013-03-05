366e9c3b7eab50801edbf2592ba1bc18f767445e
Sergio Redondo <zell.y.algo@gmail.com>
2013-03-05 10:45:16 +0100

    modified:   InterfazServidor/fich_selector 	modified:   InterfazServidor/src/reproductor/configVlcj.java

--- InterfazServidor/src/reproductor/configVlcj.java
+++ InterfazServidor/src/reproductor/configVlcj.java
@@ -2,11 +2,13 @@ package reproductor;
 
 import com.sun.jna.Native;
 import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
 import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
 public abstract class configVlcj {
 
     static {
-        Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
+        new NativeDiscovery().discover();
+//        Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
     }
 }
