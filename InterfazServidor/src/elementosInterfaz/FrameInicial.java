9381db4b9adb098948be95ad26b74aec4de1e811
Sergio Redondo <zell.y.algo@gmail.com>
2013-03-12 17:53:13 +0100

    AWESOME LOADING

--- InterfazServidor/src/elementosInterfaz/FrameInicial.java
+++ InterfazServidor/src/elementosInterfaz/FrameInicial.java
@@ -14,12 +14,13 @@ public class FrameInicial extends JFrame {
 
     public FrameInicial() {
         setSize(400, 200);
+        setUndecorated(true);
+        setVisible(true);
         getContentPane().add(new PanelInicial());
         Image icon = Imagenes.getImagen("icons/socialDJ.png").getImage();
         setIconImage(icon);
         setTitle("socialDj Loader");
         setLocation(500, 300);
-        setUndecorated(true);
-        setVisible(true);
+        
     }
 }
--- InterfazServidor/src/elementosInterfaz/PanelInicial.java
+++ InterfazServidor/src/elementosInterfaz/PanelInicial.java
@@ -7,8 +7,11 @@ package elementosInterfaz;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  *
@@ -16,15 +19,44 @@ import javax.swing.JPanel;
  */
 public class PanelInicial extends JPanel {
 
+    private int n = 0;
+    private Dimension tamanio;
+    private boolean direccion = false;
+
+    public PanelInicial() {
+
+        Timer time = new Timer(2, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (n <= tamanio.width && !direccion) {
+                    n++;
+                    if(n==tamanio.width){
+                        direccion = true;
+                    }
+                } else {
+                    n--;
+                    if(n==0){
+                        direccion = false;
+                    }
+                }
+                repaint();
+            }
+        });
+        time.start();
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Dimension tamanio = getSize();
+        tamanio = getSize();
         ImageIcon imagenFondo = Imagenes.getImagen("icons/socialDj2.png");
         if (imagenFondo != null) {
             g.setColor(Color.DARK_GRAY);
             g.fillRect(0, 0, tamanio.width, tamanio.height);
-            g.drawImage(imagenFondo.getImage(), 50, 65, tamanio.width-110, tamanio.height-130, null);
+            g.setColor(Color.LIGHT_GRAY);
+            g.fillRect(0, 0, tamanio.width - (tamanio.width - n), tamanio.height);
+            g.drawImage(imagenFondo.getImage(), 50, 65, tamanio.width - 110, tamanio.height - 130, null);
             setOpaque(false);
         } else {
             setOpaque(true);
