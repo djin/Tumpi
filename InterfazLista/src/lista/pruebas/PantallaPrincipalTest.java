0015f7125615d46afcb6035a6d041880e11e4806
Sergio Redondo <zell.y.algo@gmail.com>
2013-04-01 09:49:36 +0200

    probando robotium

---
+++ InterfazLista/src/lista/pruebas/PantallaPrincipalTest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lista.pruebas;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class PantallaPrincipalTest extends ActivityInstrumentationTestCase2 {
+    private static final String TARGET_PACKAGE_ID = "com.yourcompany.yourapp";
+    private static final String LAUNCHER_ACTIVITY_FULL_CLASSNAME = "com.yourcompany.yourapp.Activities.MainActivity";
+    private static Class<?> launcherActivityClass;
+
+}
