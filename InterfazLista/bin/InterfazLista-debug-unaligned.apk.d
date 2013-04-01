852f4873949501f98ce91ae485bad2edee917a10
Sergio Redondo <zell.y.algo@gmail.com>
2013-04-01 10:35:15 +0200

    modified:   InterfazLista/bin/AndroidManifest.xml.d 	deleted:    InterfazLista/bin/InterfazLista-debug-unaligned.apk 	deleted:    InterfazLista/bin/InterfazLista-debug-unaligned.apk.d 	deleted:    InterfazLista/bin/InterfazLista-debug.apk 	deleted:    InterfazLista/bin/InterfazLista.ap_ 	deleted:    InterfazLista/bin/InterfazLista.ap_.d 	deleted:    InterfazLista/bin/build.prop 	deleted:    InterfazLista/bin/classes.dex 	deleted:    InterfazLista/bin/classes.dex.d 	deleted:    InterfazLista/bin/classes/lista/android/BuildConfig.class 	modified:   InterfazLista/bin/classes/lista/android/PantallaPrincipal$1.class 	modified:   InterfazLista/bin/classes/lista/android/PantallaPrincipal.class 	deleted:    InterfazLista/bin/classes/lista/android/R$attr.class 	deleted:    InterfazLista/bin/classes/lista/android/R$color.class 	deleted:    InterfazLista/bin/classes/lista/android/R$drawable.class 	deleted:    InterfazLista/bin/classes/lista/android/R$id.class 	deleted:    InterfazLista/bin/classes/lista/android/R$layout.class 	deleted:    InterfazLista/bin/classes/lista/android/R$menu.class 	deleted:    InterfazLista/bin/classes/lista/android/R$raw.class 	deleted:    InterfazLista/bin/classes/lista/android/R$string.class 	deleted:    InterfazLista/bin/classes/lista/android/R$style.class 	deleted:    InterfazLista/bin/classes/lista/android/R.class 	deleted:    InterfazLista/bin/res/drawable-hdpi/ic_launcher.png 	deleted:    InterfazLista/bin/res/drawable-ldpi/ic_launcher.png 	deleted:    InterfazLista/bin/res/drawable-mdpi/ic_launcher.png 	deleted:    InterfazLista/bin/res/raw/ico_medium_list.png 	deleted:    InterfazLista/bin/res/raw/ico_medium_love.png 	deleted:    InterfazLista/bin/res/raw/ico_medium_options.png 	deleted:    InterfazLista/bin/res/raw/ico_medium_star.png 	deleted:    InterfazLista/bin/res/raw/ico_small_love.png 	deleted:    InterfazLista/bin/res/raw/ico_small_share.png 	deleted:    InterfazLista/bin/res/raw/ico_small_star_focus.png 	deleted:    InterfazLista/bin/res/raw/imgp.png 	modified:   InterfazLista/gen/R.java.d 	modified:   InterfazLista/src/lista/pruebas/PantallaPrincipalTest.java 	modified:   InterfazServidor/build/classes/conexion/ConnectionManager.class 	modified:   InterfazServidor/build/classes/conexion/SocketServidor$Cliente$1.class 	modified:   InterfazServidor/build/classes/conexion/SocketServidor$Cliente.class 	modified:   InterfazServidor/build/classes/conexion/SocketServidor.class 	modified:   InterfazServidor/build/classes/modelos/ListaCanciones.class 	modified:   InterfazServidor/fich_selector

--- InterfazLista/bin/InterfazLista-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-C:\Users\66785270\Documents\GitHub\socialDj\InterfazLista\bin\InterfazLista-debug-unaligned.apk : \
-C:\Users\66785270\Documents\GitHub\socialDj\InterfazLista\bin\InterfazLista.ap_ \
-C:\Users\66785270\Documents\GitHub\socialDj\InterfazLista\bin\classes.dex \
