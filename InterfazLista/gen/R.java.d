4f37fc1bb6933ce4ab7e29890372bc44ca6a3540
Sergio Redondo <zell.y.algo@gmail.com>
2013-04-01 10:54:09 +0200

    siguiendo pruebas

--- InterfazLista/gen/R.java.d
+++ InterfazLista/gen/R.java.d
@@ -27,4 +27,17 @@ C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\res\values\color.xml \
 C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\res\values\estiloboton.xml \
 C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\res\values\strings.xml \
 C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\res\values\styles.xml \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\drawable-hdpi\ic_launcher.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\drawable-ldpi\ic_launcher.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\drawable-mdpi\ic_launcher.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\ico_medium_list.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\ico_medium_love.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\ico_medium_options.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\ico_medium_options_focus.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\ico_medium_star.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\ico_small_love.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\ico_small_options.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\ico_small_share.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\ico_small_star_focus.png \
+C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\res\raw\imgp.png \
 C:\Users\66785320\Documents\GitHub\socialDj\InterfazLista\bin\AndroidManifest.xml \
