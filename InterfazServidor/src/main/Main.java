7df828477e3c396c0b5696595d63e51b1f753d8f
Sergio Redondo <zell.y.algo@gmail.com>
2013-04-08 09:22:40 +0200

    modified:   .gitignore 	modified:   InterfazLista/bin/classes/lista/android/AdaptadorLista$ClickListener.class 	modified:   InterfazLista/bin/classes/lista/android/AdaptadorLista.class 	modified:   InterfazLista/bin/classes/lista/android/ListaCanciones$1.class 	modified:   InterfazLista/bin/classes/lista/android/ListaCanciones$2.class 	modified:   InterfazLista/bin/classes/lista/android/ListaCanciones$3.class 	modified:   InterfazLista/bin/classes/lista/android/ListaCanciones$4.class 	modified:   InterfazLista/bin/classes/lista/android/ListaCanciones.class 	modified:   InterfazLista/bin/classes/lista/android/PantallaDatosServidor$1$1.class 	modified:   InterfazLista/bin/classes/lista/android/PantallaDatosServidor$1.class 	modified:   InterfazLista/bin/classes/lista/android/PantallaDatosServidor.class 	modified:   InterfazLista/bin/classes/lista/android/PantallaPrincipal$1.class 	modified:   InterfazLista/bin/classes/lista/android/PantallaPrincipal.class 	modified:   InterfazLista/bin/classes/lista/android/Settings.class 	modified:   InterfazServidor/build/classes/main/Main$1.class 	modified:   InterfazServidor/build/classes/main/Main.class 	modified:   InterfazServidor/fich_selector 	modified:   InterfazServidor/src/main/Main.java 	new file:   ServidorSocialDj/res/values/styles.xml 	InterfazLista/HelloWorld.jar 	InterfazLista/hs_err_pid1520.log 	InterfazLista/hs_err_pid6688.log 	ServidorSocialDj/AndroidManifest.xml 	ServidorSocialDj/ant.properties 	ServidorSocialDj/build.properties 	ServidorSocialDj/build.xml 	ServidorSocialDj/default.properties 	ServidorSocialDj/private/ 	ServidorSocialDj/proguard-project.txt 	ServidorSocialDj/project.properties 	ServidorSocialDj/res/drawable-hdpi/ 	ServidorSocialDj/res/drawable-ldpi/ 	ServidorSocialDj/res/drawable-mdpi/ 	ServidorSocialDj/res/drawable-xhdpi/ 	ServidorSocialDj/res/layout/ 	ServidorSocialDj/res/values/strings.xml 	ServidorSocialDj/src/

--- InterfazServidor/src/main/Main.java
+++ InterfazServidor/src/main/Main.java
@@ -34,14 +34,10 @@ public class Main {
                         String path, pathVlc = null;
                         try {
                             path = Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath();
-<<<<<<< HEAD
-                            pathVlc = path.replace("InterfazServidor.jar", "VLC");
-=======
 //                            pathVlc = path.replace("InterfazServidor.jar", "VLC");
                             pathVlc = path + "VLC";
                             System.out.println(pathVlc);
 //                            JOptionPane.showConfirmDialog(null, pathVlc);
->>>>>>> arreglado bug al conectar
                         } catch (URISyntaxException ex) {
                             ex.printStackTrace();
                         }
