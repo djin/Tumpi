a2590d7823c551a7faec914d63041ce9fa210d96
Sergio Redondo <zell.y.algo@gmail.com>
2013-04-08 09:24:14 +0200

    aplicacion servidor android

---
+++ ServidorSocialDj/src/interfaz/social/ListasCanciones.java
@@ -0,0 +1,15 @@
+package interfaz.social;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListasCanciones extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
