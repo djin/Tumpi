fb96879b2bac0ce0026ae1c23b415f0f6e4d1833
Juan Frances <juanfrancesgg@gmail.com>
2013-04-11 12:54:34 +0200

    Modelo lista sonando y blabla

---
+++ InterfazServidor/src/modelos/ListaSonando.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author 66786575
+ */
+public class ListaSonando {
+    
+    private ListaCanciones lista_sonando;
+    
+    public ListaSonando(ListaCanciones lista_promocionada){
+        
+    }
+}
