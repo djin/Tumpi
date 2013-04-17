231f2b0b01759a878b50111dcbfd8bbab9471600
Sergio Redondo <zell.y.algo@gmail.com>
2013-04-17 10:35:12 +0200

    añadido item

--- ServidorSocialDj/src/interfaz/social/SeleccionCanciones.java
+++ ServidorSocialDj/src/interfaz/social/SeleccionCanciones.java
@@ -31,11 +31,26 @@ public class SeleccionCanciones extends ListActivity {
         setContentView(R.layout.style_seleccionar_canciones);
         modelo = ModeloDatos.getInstance();
         datos = new ArrayList<Cancion>();
-        datos.add(new Cancion("Los Redondeles añadido", "Siempre Fuertes 2", "HUAEx34", 0, 24567, false, false));
+        for (int i = 0; i < 10; i++) {
+            datos.add(new Cancion("Los Redondeles añadido", "Siempre Fuertes 2", "HUAEx34", 0, 24567, false, false));
+        }
         adapter = new AdaptadorListaSeleccionar(this, datos, R.layout.row_style_seleccion);
-        adapter.seleccionados.add(false);
+        for(Cancion c : datos){
+            adapter.seleccionados.add(false);
+        }
         setListAdapter(adapter);
         ListView lista = (ListView) findViewById(android.R.id.list);
         lista.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if (adapter.seleccionados.get(position)) {
+            adapter.seleccionados.set(position, false);
+        } else {
+            adapter.seleccionados.set(position, true);
+        }
+        adapter.notifyDataSetChanged();
     }
 }
