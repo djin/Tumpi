585848abd1b519de1a5efeee034f53bfb155259e
Fancisco Javier Arribas Fernández <mdo@arribasjavier.com>
2013-04-25 11:11:33 +0200

    Creacion de proyecto

---
+++ BridgeSocialDj/src/bridgesocialdj/BridgeSocialDj.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bridgesocialdj;
+
+/**
+ *
+ * @author 66785270
+ */
+public class BridgeSocialDj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
