26b942536b4753ad167655329cdcfb26ba69c95b
Sergio Redondo <zell.y.algo@gmail.com>
2013-04-26 14:55:49 +0200

    cambiado diseño actionBar cliente

--- InterfazLista/gen/lista/android/R.java
+++ InterfazLista/gen/lista/android/R.java
@@ -13,11 +13,41 @@ public final class R {
     public static final class color {
         public static final int color1=0x7f060000;
         public static final int color2=0x7f060001;
+        public static final int pressed_socialtheme=0x7f060002;
     }
     public static final class drawable {
-        public static final int estiloboton1=0x7f020000;
-        public static final int estiloboton2=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ab_bottom_solid_socialtheme=0x7f020000;
+        public static final int ab_solid_socialtheme=0x7f020001;
+        public static final int ab_stacked_solid_socialtheme=0x7f020002;
+        public static final int ab_transparent_socialtheme=0x7f020003;
+        public static final int btn_cab_done_default_socialtheme=0x7f020004;
+        public static final int btn_cab_done_focused_socialtheme=0x7f020005;
+        public static final int btn_cab_done_pressed_socialtheme=0x7f020006;
+        public static final int btn_cab_done_socialtheme=0x7f020007;
+        public static final int cab_background_bottom_socialtheme=0x7f020008;
+        public static final int cab_background_top_socialtheme=0x7f020009;
+        public static final int estiloboton1=0x7f02000a;
+        public static final int estiloboton2=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int list_focused_socialtheme=0x7f02000d;
+        public static final int menu_dropdown_panel_socialtheme=0x7f02000e;
+        public static final int pressed_background_socialtheme=0x7f02000f;
+        public static final int progress_bg_socialtheme=0x7f020010;
+        public static final int progress_horizontal_socialtheme=0x7f020011;
+        public static final int progress_primary_socialtheme=0x7f020012;
+        public static final int progress_secondary_socialtheme=0x7f020013;
+        public static final int selectable_background_socialtheme=0x7f020014;
+        public static final int spinner_ab_default_socialtheme=0x7f020015;
+        public static final int spinner_ab_disabled_socialtheme=0x7f020016;
+        public static final int spinner_ab_focused_socialtheme=0x7f020017;
+        public static final int spinner_ab_pressed_socialtheme=0x7f020018;
+        public static final int spinner_background_ab_socialtheme=0x7f020019;
+        public static final int tab_indicator_ab_socialtheme=0x7f02001a;
+        public static final int tab_selected_focused_socialtheme=0x7f02001b;
+        public static final int tab_selected_pressed_socialtheme=0x7f02001c;
+        public static final int tab_selected_socialtheme=0x7f02001d;
+        public static final int tab_unselected_focused_socialtheme=0x7f02001e;
+        public static final int tab_unselected_pressed_socialtheme=0x7f02001f;
     }
     public static final class id {
         public static final int autorName=0x7f090005;
@@ -68,10 +98,22 @@ public final class R {
         public static final int settings=0x7f070001;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Socialtheme=0x7f050006;
+        public static final int ActionBar_Transparent_Socialtheme=0x7f050007;
+        public static final int ActionBarTabStyle_Socialtheme=0x7f05000a;
+        public static final int ActionButton_CloseMode_Socialtheme=0x7f05000d;
         /**  the theme applied to the application or activity 
          */
         public static final int CustomActivityTheme=0x7f050000;
         public static final int CustomTabTextStyle=0x7f050001;
+        public static final int DropDownListView_Socialtheme=0x7f050009;
+        public static final int DropDownNav_Socialtheme=0x7f05000b;
+        public static final int PopupMenu_Socialtheme=0x7f050008;
+        public static final int ProgressBar_Socialtheme=0x7f05000c;
+        public static final int Theme_Socialtheme=0x7f050005;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_Socialtheme_Widget=0x7f05000e;
         public static final int boton1=0x7f050002;
         public static final int boton2=0x7f050003;
         public static final int textStyle=0x7f050004;
