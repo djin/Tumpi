cc2768cf711e1d2ae7644f0f643bcff5e5457ab8
Sergio Redondo <zell.y.algo@gmail.com>
2013-04-30 11:54:00 +0200

    cambio estilo cliente

--- InterfazLista/gen/lista/android/R.java
+++ InterfazLista/gen/lista/android/R.java
@@ -13,72 +13,42 @@ public final class R {
     public static final class color {
         public static final int color1=0x7f060000;
         public static final int color2=0x7f060001;
-        public static final int pressed_socialclient=0x7f060002;
-        public static final int pressed_socialtheme=0x7f060003;
+        public static final int pressed_socialtheme=0x7f060002;
     }
     public static final class drawable {
-        public static final int ab_bottom_solid_socialclient=0x7f020000;
-        public static final int ab_bottom_solid_socialtheme=0x7f020001;
-        public static final int ab_solid_socialclient=0x7f020002;
-        public static final int ab_solid_socialtheme=0x7f020003;
-        public static final int ab_stacked_solid_socialclient=0x7f020004;
-        public static final int ab_stacked_solid_socialtheme=0x7f020005;
-        public static final int ab_transparent_socialclient=0x7f020006;
-        public static final int ab_transparent_socialtheme=0x7f020007;
-        public static final int btn_cab_done_default_socialclient=0x7f020008;
-        public static final int btn_cab_done_default_socialtheme=0x7f020009;
-        public static final int btn_cab_done_focused_socialclient=0x7f02000a;
-        public static final int btn_cab_done_focused_socialtheme=0x7f02000b;
-        public static final int btn_cab_done_pressed_socialclient=0x7f02000c;
-        public static final int btn_cab_done_pressed_socialtheme=0x7f02000d;
-        public static final int btn_cab_done_socialclient=0x7f02000e;
-        public static final int btn_cab_done_socialtheme=0x7f02000f;
-        public static final int cab_background_bottom_socialclient=0x7f020010;
-        public static final int cab_background_bottom_socialtheme=0x7f020011;
-        public static final int cab_background_top_socialclient=0x7f020012;
-        public static final int cab_background_top_socialtheme=0x7f020013;
-        public static final int estiloboton1=0x7f020014;
-        public static final int estiloboton2=0x7f020015;
-        public static final int ic_launcher=0x7f020016;
-        public static final int list_focused_socialclient=0x7f020017;
-        public static final int list_focused_socialtheme=0x7f020018;
-        public static final int list_view_scroll_color=0x7f020019;
-        public static final int menu_dropdown_panel_socialclient=0x7f02001a;
-        public static final int menu_dropdown_panel_socialtheme=0x7f02001b;
-        public static final int pressed_background_socialclient=0x7f02001c;
-        public static final int pressed_background_socialtheme=0x7f02001d;
-        public static final int progress_bg_socialclient=0x7f02001e;
-        public static final int progress_bg_socialtheme=0x7f02001f;
-        public static final int progress_horizontal_socialclient=0x7f020020;
-        public static final int progress_horizontal_socialtheme=0x7f020021;
-        public static final int progress_primary_socialclient=0x7f020022;
-        public static final int progress_primary_socialtheme=0x7f020023;
-        public static final int progress_secondary_socialclient=0x7f020024;
-        public static final int progress_secondary_socialtheme=0x7f020025;
-        public static final int selectable_background_socialclient=0x7f020026;
-        public static final int selectable_background_socialtheme=0x7f020027;
-        public static final int spinner_ab_default_socialclient=0x7f020028;
-        public static final int spinner_ab_default_socialtheme=0x7f020029;
-        public static final int spinner_ab_disabled_socialclient=0x7f02002a;
-        public static final int spinner_ab_disabled_socialtheme=0x7f02002b;
-        public static final int spinner_ab_focused_socialclient=0x7f02002c;
-        public static final int spinner_ab_focused_socialtheme=0x7f02002d;
-        public static final int spinner_ab_pressed_socialclient=0x7f02002e;
-        public static final int spinner_ab_pressed_socialtheme=0x7f02002f;
-        public static final int spinner_background_ab_socialclient=0x7f020030;
-        public static final int spinner_background_ab_socialtheme=0x7f020031;
-        public static final int tab_indicator_ab_socialclient=0x7f020032;
-        public static final int tab_indicator_ab_socialtheme=0x7f020033;
-        public static final int tab_selected_focused_socialclient=0x7f020034;
-        public static final int tab_selected_focused_socialtheme=0x7f020035;
-        public static final int tab_selected_pressed_socialclient=0x7f020036;
-        public static final int tab_selected_pressed_socialtheme=0x7f020037;
-        public static final int tab_selected_socialclient=0x7f020038;
-        public static final int tab_selected_socialtheme=0x7f020039;
-        public static final int tab_unselected_focused_socialclient=0x7f02003a;
-        public static final int tab_unselected_focused_socialtheme=0x7f02003b;
-        public static final int tab_unselected_pressed_socialclient=0x7f02003c;
-        public static final int tab_unselected_pressed_socialtheme=0x7f02003d;
+        public static final int ab_bottom_solid_socialtheme=0x7f020000;
+        public static final int ab_solid_socialtheme=0x7f020001;
+        public static final int ab_stacked_solid_socialtheme=0x7f020002;
+        public static final int ab_transparent_socialtheme=0x7f020003;
+        public static final int btn_cab_done_default_socialtheme=0x7f020004;
+        public static final int btn_cab_done_focused_socialtheme=0x7f020005;
+        public static final int btn_cab_done_pressed_socialtheme=0x7f020006;
+        public static final int btn_cab_done_socialtheme=0x7f020007;
+        public static final int cab_background_bottom_socialtheme=0x7f020008;
+        public static final int cab_background_top_socialtheme=0x7f020009;
+        public static final int estiloboton1=0x7f02000a;
+        public static final int estiloboton2=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int list_focused_socialtheme=0x7f02000d;
+        public static final int list_view_scroll_color=0x7f02000e;
+        public static final int menu_dropdown_panel_socialtheme=0x7f02000f;
+        public static final int pressed_background_socialtheme=0x7f020010;
+        public static final int progress_bg_socialtheme=0x7f020011;
+        public static final int progress_horizontal_socialtheme=0x7f020012;
+        public static final int progress_primary_socialtheme=0x7f020013;
+        public static final int progress_secondary_socialtheme=0x7f020014;
+        public static final int selectable_background_socialtheme=0x7f020015;
+        public static final int spinner_ab_default_socialtheme=0x7f020016;
+        public static final int spinner_ab_disabled_socialtheme=0x7f020017;
+        public static final int spinner_ab_focused_socialtheme=0x7f020018;
+        public static final int spinner_ab_pressed_socialtheme=0x7f020019;
+        public static final int spinner_background_ab_socialtheme=0x7f02001a;
+        public static final int tab_indicator_ab_socialtheme=0x7f02001b;
+        public static final int tab_selected_focused_socialtheme=0x7f02001c;
+        public static final int tab_selected_pressed_socialtheme=0x7f02001d;
+        public static final int tab_selected_socialtheme=0x7f02001e;
+        public static final int tab_unselected_focused_socialtheme=0x7f02001f;
+        public static final int tab_unselected_pressed_socialtheme=0x7f020020;
     }
     public static final class id {
         public static final int autorName=0x7f090005;
@@ -131,34 +101,22 @@ public final class R {
         public static final int settings=0x7f070001;
     }
     public static final class style {
-        public static final int ActionBar_Solid_Socialclient=0x7f050006;
-        public static final int ActionBar_Solid_Socialtheme=0x7f050010;
-        public static final int ActionBar_Transparent_Socialclient=0x7f050007;
-        public static final int ActionBar_Transparent_Socialtheme=0x7f050011;
-        public static final int ActionBarTabStyle_Socialclient=0x7f05000a;
-        public static final int ActionBarTabStyle_Socialtheme=0x7f050014;
-        public static final int ActionButton_CloseMode_Socialclient=0x7f05000d;
-        public static final int ActionButton_CloseMode_Socialtheme=0x7f050017;
+        public static final int ActionBar_Solid_Socialtheme=0x7f050006;
+        public static final int ActionBar_Transparent_Socialtheme=0x7f050007;
+        public static final int ActionBarTabStyle_Socialtheme=0x7f05000a;
+        public static final int ActionButton_CloseMode_Socialtheme=0x7f05000d;
         /**  the theme applied to the application or activity 
          */
         public static final int CustomActivityTheme=0x7f050000;
         public static final int CustomTabTextStyle=0x7f050001;
-        public static final int DropDownListView_Socialclient=0x7f050009;
-        public static final int DropDownListView_Socialtheme=0x7f050013;
-        public static final int DropDownNav_Socialclient=0x7f05000b;
-        public static final int DropDownNav_Socialtheme=0x7f050015;
-        public static final int PopupMenu_Socialclient=0x7f050008;
-        public static final int PopupMenu_Socialtheme=0x7f050012;
-        public static final int ProgressBar_Socialclient=0x7f05000c;
-        public static final int ProgressBar_Socialtheme=0x7f050016;
-        public static final int Theme_Socialclient=0x7f050005;
+        public static final int DropDownListView_Socialtheme=0x7f050009;
+        public static final int DropDownNav_Socialtheme=0x7f05000b;
+        public static final int PopupMenu_Socialtheme=0x7f050008;
+        public static final int ProgressBar_Socialtheme=0x7f05000c;
+        public static final int Theme_Socialtheme=0x7f050005;
         /**  this style is only referenced in a Light.DarkActionBar based theme 
          */
-        public static final int Theme_Socialclient_Widget=0x7f05000e;
-        public static final int Theme_Socialtheme=0x7f05000f;
-        /**  this style is only referenced in a Light.DarkActionBar based theme 
-         */
-        public static final int Theme_Socialtheme_Widget=0x7f050018;
+        public static final int Theme_Socialtheme_Widget=0x7f05000e;
         public static final int boton1=0x7f050002;
         public static final int boton2=0x7f050003;
         public static final int textStyle=0x7f050004;
