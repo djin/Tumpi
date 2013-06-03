a873f2190ecfc5a72ab0a1b77bc0c9b3b691c584
Sergio Redondo <zell.y.algo@gmail.com>
2013-06-03 12:45:01 +0200

    oldTumpi

---
+++ TumpiOldProject/TumpiOld/src/main/java/tumpiold/app/Seleccionar.java
@@ -0,0 +1,48 @@
+package tumpiold.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+import tumpiold.app.cliente.lista.android.PantallaPrincipal;
+
+public class Seleccionar extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio_app);
+        Button btnServer = (Button) findViewById(R.id.botonIniciarServidor);
+        btnServer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                Intent inte = new Intent(Seleccionar.this, PantallaPrincipal.class);
+                inte.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(inte);
+                */
+            }
+        });
+        Button btnCliente = (Button) findViewById(R.id.botonIniciarCliente);
+        btnCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inte = new Intent(Seleccionar.this, PantallaPrincipal.class);
+                inte.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(inte);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.seleccionar, menu);
+        return true;
+    }
+    
+}
