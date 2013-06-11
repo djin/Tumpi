b5d0aeed9baecf6776c6d3c907f08b3fc0211458
Sergio Redondo <zell.y.algo@gmail.com>
2013-06-11 12:20:02 +0200

    salto directamente a apliacion cliente

--- tumpiApp/src/app/tumpi/SeleccionAplicacion.java
+++ tumpiApp/src/app/tumpi/SeleccionAplicacion.java
@@ -3,6 +3,7 @@ package app.tumpi;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,7 +13,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import app.tumpi.cliente.lista.android.ListaCanciones;
 import app.tumpi.cliente.lista.android.PantallaPrincipal;
+import static app.tumpi.cliente.lista.android.PantallaPrincipal.pd;
+import app.tumpi.cliente.lista.android.conexion.ConnectionManager;
 import app.tumpi.servidor.interfaz.social.ListaPromocionada;
 
 /**
@@ -21,6 +25,10 @@ import app.tumpi.servidor.interfaz.social.ListaPromocionada;
  */
 public class SeleccionAplicacion extends Activity {
 
+    public SeleccionAplicacion() {
+        ConnectionManager conex;
+    }
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,19 +37,49 @@ public class SeleccionAplicacion extends Activity {
         action.hide();
         Button btnClient = (Button) findViewById(R.id.botonIniciarCliente);
         btnClient.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+            public void onClick(final View v) {
                 final AlertDialog.Builder alert = new AlertDialog.Builder(SeleccionAplicacion.this);
                 LayoutInflater inflater = (LayoutInflater) SeleccionAplicacion.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 final View vi = inflater.inflate(R.layout.style_view_nombre_servidor, null, false);
                 alert.setView(vi);
                 alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    private ConnectionManager conex;
                     public void onClick(DialogInterface dialog, int whichButton) {
                         EditText input = (EditText) vi.findViewById(R.id.txtInputNombreServidor);
-                        String value = input.getText().toString();
+                        final String value = input.getText().toString();
                         if (!value.equals("")) {
-                            Intent inte = new Intent(SeleccionAplicacion.this, PantallaPrincipal.class);
-                            inte.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                            startActivity(inte);
+                            pd = ProgressDialog.show(SeleccionAplicacion.this, "Conectando", "Espere unos segundos...", true, false);
+                            conex = new ConnectionManager();
+                            v.postDelayed(new Runnable() {
+                                public void run() {
+                                    try {
+                                        if (conex.conectar() && conex.logInBridge(value)) {
+                                            conex.conexion.startListeningServer();
+                                            Intent inte = new Intent(SeleccionAplicacion.this, ListaCanciones.class);
+                                            startActivity(inte);
+                                        } else {
+                                            pd.dismiss();
+                                            conex.conexion.cerrarConexion();
+                                            conex = null;
+                                            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(SeleccionAplicacion.this);
+                                            dialogBuilder.setMessage("No existe un servidor publicado con este nombre o no tiene conexion con la central de socialDj");
+                                            dialogBuilder.setTitle("Error de conexion");
+                                            dialogBuilder.setPositiveButton("Aceptar", new android.content.DialogInterface.OnClickListener() {
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    dialog.dismiss();
+                                                }
+                                            });
+                                            dialogBuilder.show();
+                                        }
+
+                                    } catch (Exception ex) {
+                                        pd.dismiss();
+                                        conex = null;
+                                        Toast.makeText(SeleccionAplicacion.this, "Ha ocurrido un error al intentar conectar, intentelo mas tarde: \n" + ex.toString(), Toast.LENGTH_LONG).show();
+                                    }
+                                    pd.dismiss();
+                                }
+                            }, 100);
                         } else {
                             Toast.makeText(alert.getContext(), "Escriba un nombre para el servidor", Toast.LENGTH_SHORT).show();
                             //dialog.cancel();
